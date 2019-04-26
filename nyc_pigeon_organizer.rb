f77184f1869fbbafab2d7f57d7be38f0e2f64724
fut0k0 <cccabell@icloud.com>
2019-04-26 09:55:24 -0700

    Passing lab

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	#create new hash to hold data
+	new_hash ={}
+	
+	#create first-level keys of new hash
+	data[:gender].each do |gender, bird|
+		bird.each {|name| new_hash[name] = {}}
+	end	
+	
+	#populate new hash with data
+	new_hash.each do |bird, stats|
+		data.each do |attribute, value|
+			value.each do |detail, name|
+				if name.include?(bird)
+					new_hash[bird][attribute] ||= []
+					new_hash[bird][attribute].push(detail.to_s)
+				end	
+			end	
+			
+		end	
+	end	
+	
+	new_hash
 end
